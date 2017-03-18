e512b24c85a081275af3dee3e57532222a28b4da
Babatunde Fashola <bfashola@twitter.com>
2017-03-18 16:21:33 -0700

    More testing of the Library

--- Dimple.Scalajs.Example/src/main/scala/org/scalajs/dimplejs/DataFromFile.scala
+++ Dimple.Scalajs.Example/src/main/scala/org/scalajs/dimplejs/DataFromFile.scala
@@ -1,22 +1,47 @@
 package org.scalajs.dimplejs
 
-import org.scala.dimplejs.{Chart, Dimple, PlotLine}
+import org.scala.dimplejs._
 import org.singlespaced.d3js.d3
 import scala.scalajs.js
 
 class DataFromFile {
 
   def visualize(): Unit = {
-    val callback: js.Function1[js.Array[js.Dictionary[String]], Unit] = (tsvData: js.Array[js.Dictionary[String]]) => {
+
+    //Create the CallBack Function For D3
+    val callback: js.Function1[js.Array[js.Dictionary[String]], Unit]
+    = (tsvData: js.Array[js.Dictionary[String]]) => {
+
+      //Create the SVG using Dimple
       val svg = Dimple.newSvg("#lineContainer", 590, 400)
+
+      //Filter out the unneeded data from the file.
       val filteredData = Dimple.filterData(tsvData, "Owner", js.Array("Aperture", "Black Mesa"))
-      val myChart = new Chart(svg, filteredData)
-      myChart.setBounds(60, 30, 505, 305)
-      val x = myChart.addCategoryAxis("x", "Month")
+
+      //Create the Chart where the SVG should be displayed
+      val chart = new Chart(svg, filteredData)
+
+      //Set the Bounds of the Chart
+      chart.setBounds(60, 30, 505, 305)
+
+      //Create the X-Axis on the Chart
+      val x = chart.addCategoryAxis("x", "Month")
       x.addOrderRule("Date")
-      myChart.addMeasureAxis("y", "Unit Sales")
-      val s = myChart.addSeries(plotFunction = PlotLine)
-      myChart.draw()
+
+      //Create the y-axis of the Chart
+      chart.addMeasureAxis("y", "Unit Sales")
+
+      //Set the Plot Function of the Chart
+      chart.addSeries(plotFunction = PlotBar)
+
+      //Create the Color Object for the Chart
+      val color = new Color(fill = "#333333", opacity = 0.2F)
+      chart.defaultColors = js.Array(color)
+
+      //Call the draw funsction to draw the Chart
+      chart.draw()
+
+      //Return a dummy Unit
       ()
     }
     d3.tsv("./data/example_data.tsv", callback)
